76d71e06ba7aad1d2635c3bd28cba4989515af4f
arvin <h_chaoran@163.com>
2022-04-27 19:20:34 +0800

    用户中心-头像上传与修改V1.0

--- food-dev-aip/src/main/java/com/hcr/center/CenterUsersController.java
+++ food-dev-aip/src/main/java/com/hcr/center/CenterUsersController.java
@@ -2,21 +2,30 @@ package com.hcr.center;
 
 import com.hcr.bo.center.CenterUserBO;
 import com.hcr.pojo.Users;
+import com.hcr.resource.FileUpload;
 import com.hcr.service.center.CenterUserService;
 import com.hcr.utils.CookieUtils;
+import com.hcr.utils.DateUtils;
 import com.hcr.utils.JSONResult;
 import com.hcr.utils.JsonUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +38,79 @@ public class CenterUsersController {
     @Autowired
     private CenterUserService centerUserService;
 
+    @Autowired
+    private FileUpload fileUpload;
+
+    @ApiOperation(value = "用户头像修改", notes = "用户头像修改", httpMethod = "POST")
+    @PostMapping("uploadFace")
+    public JSONResult uploadFace(
+            @ApiParam(name = "userId", value = "用户id", required = true)
+            @RequestParam String userId,
+            @ApiParam(name = "file", value = "用户头像", required = true)
+            MultipartFile file,
+            HttpServletRequest request,HttpServletResponse response) throws IOException {
+
+        //定义用户头像地址
+        String fileSpace = fileUpload.getImageUserFaceLocation();
+        //在路径上为每一个用户增加一个userid，用于区分用户上传
+        //File.separator 代替分隔符 "/" 因 Windows 与 Linux 不相同  D:\\fhb\\test\\"
+        String uploadPathPrefix = File.separator + userId;
+
+        //开始文件上传
+        if (file != null){
+            FileOutputStream fileOutputStream = null;
+            try {
+
+                //获得文件上传的文件名称，原始文件名 getOriginalFilename
+                String fileName = file.getOriginalFilename();
+                if (StringUtils.isNotBlank(fileName)){
+
+                    //文件重命名 123-face.png -> ["123-face","png"]
+                    String fileNameArr[] = fileName.split("\\.");
+
+                    //获取文件的后缀名  索引下标是以0开始的，所以-1
+                    String suffix = fileNameArr[fileNameArr.length - 1];
+
+                    if (!suffix.equalsIgnoreCase("png") &&
+                        !suffix.equalsIgnoreCase("jpg") &&
+                        !suffix.equalsIgnoreCase("jpeg")){
+                        return JSONResult.errorMsg("图片格式不正确!");
+                    }
+
+                    //face-{userid}.png
+                    //文件名程重组 覆盖试上传， 增量式上传：额外拼接当前时间
+                    String newFileName = "face-" + userId + DateUtils.getTimeNum() + "." + suffix;
+                    //上传的头像最终保存的位置
+                    String finalFacePath = fileSpace + uploadPathPrefix + File.separator + newFileName;
+
+                    File outFile = new File(finalFacePath);
+                    if (outFile.getParentFile() != null) {
+                        // 创建文件夹
+                        outFile.getParentFile().mkdirs();
+                    }
+                    //文件输出保存到目录
+                    fileOutputStream = new FileOutputStream(outFile);
+                    InputStream inputStream = file.getInputStream();
+                    IOUtils.copy(inputStream,fileOutputStream);
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (fileOutputStream != null) {
+                        fileOutputStream.flush();
+                        fileOutputStream.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return JSONResult.ok();
+    }
+
     @ApiOperation(value = "修改用户信息", notes = "修改用户信息", httpMethod = "POST")
     @PostMapping("update")
     public JSONResult update(
@@ -38,6 +120,7 @@ public class CenterUsersController {
             BindingResult result,
             HttpServletResponse response, HttpServletRequest request){
 
+        // @Valid 验证注解     BindingResult result -> 错误信息
         // 判断BindingResult是否保存错误的验证信息，如果有，则直接return
         if (result.hasErrors()) {
             Map<String, String> errorMap = getErrors(result);
---
+++ food-dev-aip/src/main/java/com/hcr/resource/FileUpload.java
@@ -0,0 +1,33 @@
+package com.hcr.resource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+//组件让springboot扫描
+@Component
+//属性资源文件配置 前缀
+@ConfigurationProperties(prefix = "file")
+//项目打包resource文件都在class path下，切换环境发布项目只需将prod更改即可
+@PropertySource("classpath:file-upload-prod.properties")
+public class FileUpload {
+
+    private String imageUserFaceLocation;
+    private String imageServerUrl;
+
+    public String getImageUserFaceLocation() {
+        return imageUserFaceLocation;
+    }
+
+    public void setImageUserFaceLocation(String imageUserFaceLocation) {
+        this.imageUserFaceLocation = imageUserFaceLocation;
+    }
+
+    public String getImageServerUrl() {
+        return imageServerUrl;
+    }
+
+    public void setImageServerUrl(String imageServerUrl) {
+        this.imageServerUrl = imageServerUrl;
+    }
+}
