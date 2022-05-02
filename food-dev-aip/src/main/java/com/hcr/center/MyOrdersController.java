b1544e1ac10be127a0f214a48dd5b1686c434b38
arvin <h_chaoran@163.com>
2022-05-02 18:17:24 +0800

    用户中心-查询订单列表

--- food-dev-aip/src/main/java/com/hcr/center/MyOrdersController.java
+++ food-dev-aip/src/main/java/com/hcr/center/MyOrdersController.java
@@ -1,17 +1,27 @@
 package com.hcr.center;
 
 import com.hcr.controller.BaseController;
+import com.hcr.service.center.MyOrdersService;
 import com.hcr.utils.JSONResult;
+import com.hcr.utils.PagedGridResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping
+
 @Api(value = "用户中心我的订单", tags = {"用户中心我的订单相关接口"})
+@RestController
+@RequestMapping("myorders")
 public class MyOrdersController extends BaseController {
+
+    @Autowired
+    private MyOrdersService myOrdersService;
 
     @ApiOperation(value = "获取订单状态数据概况", notes = "获取订单状态数概况", httpMethod = "POST")
     @PostMapping("/statusCounts")
@@ -19,4 +29,29 @@ public class MyOrdersController extends BaseController {
         return null;
 
     }
+
+    @ApiOperation(value = "查询订单列表", notes = "查询订单列表", httpMethod = "POST")
+    @PostMapping("/query")
+    public JSONResult query(  @ApiParam(name = "userId", value = "用户id", required = true)
+                              @RequestParam String userId,
+                              @ApiParam(name = "orderStatus", value = "订单状态", required = false)
+                              @RequestParam Integer orderStatus,
+                              @ApiParam(name = "page", value = "查询下一页的第几页", required = false)
+                              @RequestParam Integer page,
+                              @ApiParam(name = "pageSize", value = "分页的每一页显示的条数", required = false)
+                              @RequestParam Integer pageSize) {
+        if (StringUtils.isBlank(userId)){
+            return JSONResult.errorMsg(null);
+        }
+        if (page == null){
+            page = 1;
+        }
+        if (pageSize == null){
+            pageSize = COMMON_PAGE_SIZE;
+        }
+        PagedGridResult gridResult = myOrdersService.queryMyOrders(userId, orderStatus, page, pageSize);
+        return JSONResult.ok(gridResult);
+    }
+
 }
+
