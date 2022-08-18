b74187c488b05bb1dc8b70f0e2ed4679948996ca
arvin <h_chaoran@163.com>
2022-08-19 01:13:09 +0800

    基于Redisson实现分布式锁

--- food-dev-service/src/main/java/com/hcr/service/impl/ItemServiceImpl.java
+++ food-dev-service/src/main/java/com/hcr/service/impl/ItemServiceImpl.java
@@ -13,6 +13,8 @@ import com.hcr.vo.CommentLevelCountsVO;
 import com.hcr.vo.ItemCommentVO;
 import com.hcr.vo.SearchItemsVO;
 import com.hcr.vo.ShopCartVO;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -20,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -41,6 +44,9 @@ public class ItemServiceImpl implements ItemService {
 
     @Autowired
     private ItemsMapperCustom itemsMapperCustom;
+
+    @Autowired
+    private RedissonClient redisson;
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -174,11 +180,38 @@ public class ItemServiceImpl implements ItemService {
     @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void decreaseItemSpecStock(String specId, int buyCounts) {
+        /**
+         * 这里需要注意的是获取锁时传入的key，
+         * 这里采用的是商品的规格ID，在并发时，规则ID相同时，才会产生等待
+         */
+        RLock rLock = redisson.getLock("SPECID_"+specId);
+        try {
+            /**
+             * 1、获取分布式锁，锁的超时时间是5秒get
+             *  2、获取到了锁，进行后续的业务操作
+             */
+            rLock.lock(5, TimeUnit.HOURS);
 
-        int result = itemsMapperCustom.decreaseItemSpecStock(specId,buyCounts);
+            int result = itemsMapperCustom.decreaseItemSpecStock(specId, buyCounts);
+            if (result != 1) {
+                throw new RuntimeException("订单创建失败，原因：库存不足!");
+            }
+        } catch (Exception e) {
+          //  logger.error(e.getMessage(),e);
+            throw new RuntimeException(e.getMessage(),e);
+        }finally {
+            /**
+             *  不管业务是否操作正确，随后都要释放掉分布式锁
+             *   如果不释放，过了超时时间也会自动释放
+             */
+            rLock.unlock();
+        }
+
+
+        /*int result = itemsMapperCustom.decreaseItemSpecStock(specId,buyCounts);
         if (result != 1){
             throw new RuntimeException("订单创建失败：原因：库存不足！");
-        }
+        } */
     }
 
     /**
